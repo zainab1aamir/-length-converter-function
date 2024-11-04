a5da1cfe7dbf9223f5de3ad4d5cac73466d164c2
Zainab Aamir <zaamir427@gmail.com>
2024-11-04 21:13:03 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// Creating a function is a skill that’ll be useful in many settings, and as you progress, you’ll be working on much more complicated functions than this one. But a function that converts units of measure can be pretty handy in multiple professions and industries.
+
+// Let’s start with a conversion from kilometers to miles. The function should include the input in kilometers and return the answer in miles.
+
+
+
+function myFunction(){
+    let number = prompt("enter the distance in kilometer: ");
+    let result = number * 0.621371;
+    console.log(result);
+    
+};
+
+myFunction();
+
+
+
+
+
+
+
+
+
+
+
+
+
